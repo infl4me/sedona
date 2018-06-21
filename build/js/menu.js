209efa9c8504b8dc077993cf476ff2b14e0ca23a
infl4me <hellpl4y@gmail.com>
2018-06-21 09:10:21 +0500

    =

---
+++ build/js/menu.js
@@ -0,0 +1,1 @@
+var navMain=document.querySelector(".main-nav"),navToggle=document.querySelector(".main-nav__toggle");navMain.classList.add("main-nav--no-js"),navToggle.addEventListener("click",function(){navMain.classList.toggle("main-nav--no-js")});
